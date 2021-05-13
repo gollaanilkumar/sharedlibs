9fc1ac3e66dc5c68241f2938d70b2d39fa7d5b76
gollaanilkumar <gollaanilkumar518@gmail.com>
2021-05-13 13:19:47 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -2,7 +2,7 @@ def call(cred_id,user,ip)
 {
   sshagent([cred_id]) {
     // some block
-    sh "mv target/myweb*.war target/deployshare.war"
+    sh "mv target/feature*.war target/deployshare.war"
     scp " -o StrictHostChecking=no  target/deploysahre.war ${user}@${ip}:/opt/tomcat8/webapps"
     ssh "${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
     ssh "${user}@${ip} /opt/tomcat8/bin/startup.sh"
