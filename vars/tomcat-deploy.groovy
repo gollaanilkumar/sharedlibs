39069edf71d63d928c52d052b0c174be3325554d
gollaanilkumar <gollaanilkumar518@gmail.com>
2021-05-13 12:45:59 +0530

    Create tomcat-deploy.groovy

---
+++ vars/tomcat-deploy.groovy
@@ -0,0 +1,10 @@
+def call(cred_id,user,ip)
+{
+  sshagent([cred_id]) {
+    // some block
+    sh "mv target/myweb*.war target/deployshare.war
+    scp " -o StrictHostChecking=no  target/deploysahre.war ${user}@${ip}:/opt/tomcat8/webapps"
+    ssh "${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
+    ssh "${user}@${ip} /opt/tomcat8/bin/startup.sh"
+    echo "deployed to tomcat"
+}
